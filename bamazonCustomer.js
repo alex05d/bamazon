a350a543a6f89f49eb5c106cfd2b0d451ec63a21
Alexiss Diaz <alexiss.diaz@live.com>
2019-12-04 22:16:00 -0600

    added final logic to app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,110 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "",
+    database: "products_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "Store",
+            type: "list",
+            message: 'Would you like to shop?',
+            choices: ['YES!', 'No']
+        })
+        .then(function (answer) {
+            if (answer.Store === "YES!") {
+                showProducts();
+                openStore();
+            }
+            else {
+                console.log('\nThank you come again!\n');
+                connection.end();
+            }
+        })
+};
+
+function showProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, rows) {
+        if (err) throw err;
+
+        rows.forEach(function (results) {
+            console.log('Id:', results.id, '-', results.product_name, '-', 'Price: ', + results.price);
+        });
+
+    });
+
+};
+
+function openStore() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "item",
+                    type: "input",
+                    message: "Please enter the ID of the item you would like to buy!"
+                },
+                {
+                    name: "amount",
+                    type: "input",
+                    message: "how many would you like?"
+                }
+            ])
+            .then(function (answer) {
+                var query = "SELECT id, product_name, price, stock_quantitiy FROM products WHERE ?";
+                connection.query(query, { id: answer.item }, function (err, res) {
+                    for (var i = 0; i < res.length; i++) {
+                        if (parseInt(answer.amount) > parseInt(res[i].stock_quantitiy)) {
+
+                            console.log("\n Not enough left in stock! \n");
+                            start();
+
+                        } else {
+                            var newInv = parseInt(res[i].stock_quantitiy) - parseInt(answer.amount);
+                            var total = parseInt(res[i].price) * parseInt(answer.amount);
+
+                            var query = connection.query(
+                                "UPDATE products SET ? WHERE ?",
+                                [
+                                    {
+                                        stock_quantitiy: newInv
+                                    },
+                                    {
+                                        id: res[i].id
+                                    }
+                                ]
+                            );
+                            console.log("\nYou purchased " + answer.amount + res[i].product_name + " at the price of $ " + res[i].price);
+                            console.log("\nThank you for your purchase your total is $ " + total + "\n");
+
+                            start();
+                        }
+                    }
+
+                });
+            });
+
+    })
+}
+
+
+
+
--- productSeed.sql
+++ productSeed.sql
@@ -19,3 +19,71 @@ CREATE TABLE products
     (id)
 );
 
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Brisket","Meat",30.00,
+    15
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Chicken","Meat",
+    15.00,
+    13
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Pork","Meat",
+    20.00,
+    11
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Turkey","Meat",
+    12.00,
+    5
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Broccoli","Veggies",
+    2.50,
+    30
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Lettuce","Veggies",3.00,
+    8
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Carrots","Veggies",
+    1.00,
+    10
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Apples","Fruit",
+    .50,
+    2
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Grapes","Fruit",
+    2.00,
+    20
+    );
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantitiy)
+    VALUE
+    ("Bananas","Fruit",
+    4.00,
+    15
+    );
